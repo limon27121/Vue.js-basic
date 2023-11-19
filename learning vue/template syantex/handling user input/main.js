eeb3b13141c48a92e88ceffec5468f93f7afc17d
Hunter <moontaser009@gmail.com>
2023-11-19 18:45:32 +0600

    reaction timer added

---
+++ learning vue/template syantex/handling user input/main.js
@@ -0,0 +1,28 @@
+
+    // import { createApp, ref } from 'vue'
+    
+    // createApp({
+    //   setup() {
+    //     const message = ref('Hello World!')
+    
+    //     function reverseMessage() {
+    //       // Access/mutate the value of a ref via
+    //       // its .value property.
+    //       message.value = message.value.split('').reverse().join('')
+    //     }
+    
+    //     function notify() {
+    //       alert('navigation was prevented.')
+    //     }
+    //     function add(){
+    //       message.value+= '!'
+    //     }
+    
+    //     return {
+    //       message,
+    //       reverseMessage,
+    //       notify,
+    //       add
+    //     }
+    //   }
+    // }).mount('#app')
