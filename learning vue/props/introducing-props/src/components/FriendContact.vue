e10d1bf2ef85ac1d412a2c55007596929ab4e88e
Hunter <moontaser009@gmail.com>
2023-12-06 13:19:54 +0600

    props and hhtp requsted content updated

---
+++ learning vue/props/introducing-props/src/components/FriendContact.vue
@@ -0,0 +1,43 @@
+<template>
+  <li>
+    <h2>{{ name }}</h2>
+    <button @click="toggleDetails">{{ detailsAreVisible ? 'Hide' : 'Show' }} Details</button>
+    <ul v-if="detailsAreVisible">
+      <li>
+        <strong>Phone:</strong>
+        {{ phoneNumber }}
+      </li>
+      <li>
+        <strong>Email:</strong>
+        {{ emailAddress }}
+      </li>
+    </ul>
+  </li>
+</template>
+
+<script>
+export default {
+  // catch the props
+  props: [
+    'name',
+    'phoneNumber',
+    'emailAddress'
+  ],
+  data() {
+    return {
+      detailsAreVisible: false,
+      friend: {
+        id: "manuel",
+        name: "Manuel Lorenz",
+        phone: "0123 45678 90",
+        email: "manuel@localhost.com",
+      },
+    };
+  },
+  methods: {
+    toggleDetails() {
+      this.detailsAreVisible = !this.detailsAreVisible;
+    }
+  }
+};
+</script>
---
+++ learning vue/props/working-with-dynamic-prop-values/src/components/FriendContact.vue
@@ -0,0 +1,64 @@
+<template>
+  <li>
+    <h2>{{ name }} {{ friendIsFavorite ? '(Favorite)' : ''}}</h2>
+    
+    <button @click="toggleFavorite">Toggle Favorite</button>
+    <!-- dynamic button name -->
+    <button @click="toggleDetails">{{ detailsAreVisible ? 'Hide' : 'Show' }} Details</button>
+    <ul v-if="detailsAreVisible">
+      <li>
+        <strong>Phone:</strong>
+        {{ phoneNumber }}
+      </li>
+      <li>
+        <strong>Email:</strong>
+        {{ emailAddress }}
+      </li>
+    </ul>
+  </li>
+</template>
+
+<script>
+export default {
+  //array of props
+  // props: ['name', 'phoneNumber', 'emailAddress', 'isFavorite'],
+  //object of props
+  props: {
+    name: {
+      type: String,
+      required: true,
+    },
+    phoneNumber: {
+      type: String,
+      required: true
+    },
+    emailAddress: {
+      type: String,
+      required: true
+    },
+    isFavorite: {
+      type: Boolean,
+      required: false,
+      default: false,
+      // validator: function(value) {
+      //   return value === '1' || value === '0';
+      // }
+    },
+  },
+  data() {
+    return {
+      detailsAreVisible: false,
+      //hold the props data
+      friendIsFavorite: this.isFavorite,
+    };
+  },
+  methods: {
+    toggleDetails() {
+      this.detailsAreVisible = !this.detailsAreVisible;
+    },
+    toggleFavorite() {
+      this.friendIsFavorite = !this.friendIsFavorite;
+    },
+  },
+};
+</script>
