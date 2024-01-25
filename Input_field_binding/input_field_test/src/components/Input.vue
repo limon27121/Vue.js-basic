126c42eb3d66f5d5846f119381306cb654b2b3a7
limon27121 <moontaser009@gmail.com>
2024-01-25 15:50:58 +0600

    input_bind_ added

---
+++ Input_field_binding/input_field_test/src/components/Input.vue
@@ -0,0 +1,49 @@
+<script setup>
+import { ref } from 'vue'
+
+let textInputValue=ref("");
+let checkboxValues=ref([]);
+let radioValue=ref("");
+let selectedOption=ref();
+</script>
+
+<template>
+<div>
+    <label for="textInput">Text Input:</label>
+    <input type="text" id="textInput" v-model="textInputValue" />
+    <br>
+    <p>{{ textInputValue}}</p>
+</div>
+<div>
+    <label for="checkbox1">Checkbox1:</label>
+    <input type="checkbox" id="checkbox1" v-model="checkboxValues[0]" />
+
+    <label for="checkbox2">Checkbox2:</label>
+    <input type="checkbox" id="checkbox2" v-model="checkboxValues[1]" />
+
+    <p>Checkbox1 Value: {{ checkboxValues[0] }}</p>
+    <p>Checkbox2 Value: {{ checkboxValues[1] }}</p>
+  </div>
+
+<div>
+    <label>Radio Buttons:</label>
+    <div>
+      <input type="radio" id="radioOption1" value="rice" v-model="radioValue" />
+      <label for="radioOption1">Rice</label>
+
+      <input type="radio" id="radioOption2" value="Egg" v-model="radioValue" />
+      <label for="radioOption2">Egg</label>
+    </div>
+    <p>{{ radioValue }}</p>
+</div>
+<div>
+    <label for="selectBox">Select Box:</label>
+    <select id="selectBox" v-model="selectedOption">
+      <option value="option1">Option 1</option>
+      <option value="option2">Option 2</option>
+      <option value="option3">Option 3</option>
+    </select>
+
+    <p>Selected Option: {{ selectedOption }}</p>
+</div>
+</template>
---
+++ Input_field_binding/input_field_test/src/components/Test.vue
@@ -0,0 +1,44 @@
+<script setup>
+import { ref,reactive } from 'vue'
+let url="images.app.goo.gl/vUS7jJPTxvJD7Q5v6"
+let selected=ref([])
+let check=ref();
+let persons=reactive([
+    {name:"limon",age:26,work:"developer"},
+    {name:"fahim",age:25,work:"tester"}
+])
+let color=ref();
+function get_style(){
+    return{
+    backgroundColor: color.value,
+    }
+}
+</script>
+<template>
+    <select v-model="selected">
+  <option v-for="person in persons" :value="{ age: person.age, name: person.name }">
+    {{ person.name }}
+  </option>
+</select>
+<p>name is:{{ selected.name }}</p>
+<p>age is:{{ selected.age }}</p>
+
+<div>
+<label for="">enter color</label> <input type="text" v-model="color">
+</div>
+<div :style="get_style()">
+<p :style="{ color: color === 'white' ? 'black' : 'white' }">here is the section</p>
+</div>
+
+<input type="radio" id="one" :value="true" v-model="check" />
+<label for="one">One</label>
+
+<input type="radio" id="two" :value="false" v-model="check" />
+<label for="two">Two</label>
+
+<div>
+ <img :src="url" alt="No Image">
+</div>
+</template>
+<style scoped>
+</style>
