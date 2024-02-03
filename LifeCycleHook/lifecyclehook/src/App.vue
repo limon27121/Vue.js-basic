75ec55500fec6100fe7f06225454fa93914b6ad4
limon <moontaser009@gmail.com>
2024-02-03 14:31:23 +0600

    lifecyclehook basic added in composition api

---
+++ LifeCycleHook/lifecyclehook/src/App.vue
@@ -0,0 +1,33 @@
+<script setup>
+import HelloWorld from './components/HelloWorld.vue'
+import { ref,onMounted,onBeforeMount,onBeforeUpdate,onUpdated } from 'vue'
+const count=ref(0);
+const source=ref(true);
+onBeforeMount(()=>{
+  console.log("1.beforeMount");
+})
+
+onMounted(()=>{
+  console.log("2.Mounted");
+})
+
+onBeforeUpdate(()=>{
+  console.log("3.before updated");
+})
+
+onUpdated(()=>{
+  console.log("4.updated");
+})
+
+</script>
+
+<template>
+  <h1>parrent</h1>
+  <button type="button" @click="count++">count is {{ count }}</button><br>
+  <button @click="source=!source">toggle</button>
+  <HelloWorld v-if="source"/>
+</template>
+
+<style scoped>
+
+</style>
---
+++ LifeCycleHook/lifecyclehook/src/components/HelloWorld.vue
@@ -0,0 +1,23 @@
+<script setup>
+import { onMounted, onBeforeMount, onUnmounted,onBeforeUnmount } from 'vue';
+
+onBeforeUnmount(()=>{
+  console.log("5.before unmount");
+})
+
+onUnmounted(()=>{
+  console.log("6.unmounted")
+})
+
+</script>
+
+<template>
+  <h1>Child</h1>
+
+</template>
+
+<style scoped>
+.read-the-docs {
+  color: #888;
+}
+</style>
